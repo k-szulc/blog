37c78d7ab865e0c1001ea47806a348b507ca0966
s-q-n <sqn@protonmail.com>
2017-12-08 08:24:45 +0100

    Create BlogPost model, added adminsite and admin user

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class BlogPost(models.Model):
+    """A model for Post, that user will add to his blog"""
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """String representation"""
+        return self.title
