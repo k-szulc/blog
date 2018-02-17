48a14e4c1717916201402737731b08b9b2557514
s-q-n <sqn@protonmail.com>
2018-02-17 18:08:28 +0100

    Fixed new_post and created token generator + set up email constraints in settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -128,6 +128,12 @@ STATIC_URL = '/static/'
 
 LOGIN_URL = '/users/login/'
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sqnblog.superuser@gmail.com'
+EMAIL_HOST_PASSWORD = 'Katiusza@#23'
+EMAIL_PORT = 587
+
 BOOTSTRAP3 = {
     'include_jquery': True,
     }
